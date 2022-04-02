70c3c03b19ef7dc58c7297720cdc7cd7de16ebc2
Abdellah El azraoui <ael-azra@e1r12p4.1337.ma>
2022-04-02 17:48:16 +0000

    create vector header (simple header)

--- Vector.hpp
+++ Vector.hpp
@@ -6,19 +6,26 @@
 /*   By: ael-azra <ael-azra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/02 14:34:41 by ael-azra          #+#    #+#             */
-/*   Updated: 2022/04/02 16:51:51 by ael-azra         ###   ########.fr       */
+/*   Updated: 2022/04/02 17:47:31 by ael-azra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include <iostream>
+#include <memory>
 
 namespace ft
 {
-	template <typename Type>
+	typedef long long ll;
+	template <typename Type, typename Alloc = std::allocator<Type> >
 	class Vector
 	{
+		private:
+			Type*	arr;
+			ll		capacity;
+			ll		length;
+
 		public:
 			Vector();
 			Vector(Vector const & obj);
