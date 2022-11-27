9cd66a2170c98e15b840fd76060d71796dd2c7f7
Abdellah El Azraoui <ael-azra@student.1337.ma>
2022-11-27 11:09:46 +0100

    abdellah

--- vector/utils.hpp
+++ vector/utils.hpp
@@ -21,7 +21,7 @@ namespace ft
 	bool lexicographical_compare( InputIt1 first1, InputIt1 last1, InputIt2 first2, InputIt2 last2 ) {
 		while (first1 != last1 && first2 != last2)
 		{
-			if (*first1 < first2)
+			if (*first1 < *first2)
 				return true;
 			else if (*first1 > *first2)
 				return false;
