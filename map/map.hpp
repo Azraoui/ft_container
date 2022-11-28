471be41c1fb7c90aa5a13bec78ca2070be04d345
Abdellah El Azraoui <ael-azra@student.1337.ma>
2022-11-28 18:41:10 +0100

    abdellah

--- map/map.hpp
+++ map/map.hpp
@@ -5,9 +5,13 @@
 namespace ft
 {
 	template <typename key, typename value, typename Compare = std::less<key>,
-	typename Alloc = std::allocator<std::pair<const key, value> > >
+	typename Alloc = std::allocator<ft::pair<const key, value> > >
 	class map
 	{
+		// Member types
+		public:
+			typedef	key				key_type;
+			typedef	value			mapped_type;
 
 	};
 }
