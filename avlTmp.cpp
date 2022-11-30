85c592db3c9951b245969dcf9064cbca6ccefc47
Abdellah El Azraoui <ael-azra@student.1337.ma>
2022-11-30 02:24:07 +0100

    abdellah

--- avlTmp.cpp
+++ avlTmp.cpp
@@ -1,5 +1,6 @@
-// C++ program to insert a node in AVL tree
-#include<iostream>
+// C++ program to delete a node from AVL Tree
+// #include<bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 // An AVL tree node
@@ -12,8 +13,12 @@ class Node
 	int height;
 };
 
-// A utility function to get the
-// height of the tree
+// A utility function to get maximum
+// of two integers
+int max(int a, int b);
+
+// A utility function to get height
+// of the tree
 int height(Node *N)
 {
 	if (N == NULL)
@@ -91,15 +96,13 @@ int getBalance(Node *N)
 {
 	if (N == NULL)
 		return 0;
-	return height(N->left) - height(N->right);
+	return height(N->left) -
+		height(N->right);
 }
 
-// Recursive function to insert a key
-// in the subtree rooted with node and
-// returns the new root of the subtree.
 Node* insert(Node* node, int key)
 {
-	/* 1. Perform the normal BST insertion */
+	/* 1. Perform the normal BST rotation */
 	if (node == NULL)
 		return(newNode(key));
 
@@ -107,20 +110,20 @@ Node* insert(Node* node, int key)
 		node->left = insert(node->left, key);
 	else if (key > node->key)
 		node->right = insert(node->right, key);
-	else // Equal keys are not allowed in BST
+	else // Equal keys not allowed
 		return node;
 
 	/* 2. Update height of this ancestor node */
 	node->height = 1 + max(height(node->left),
 						height(node->right));
 
-	/* 3. Get the balance factor of this ancestor
-		node to check whether this node became
-		unbalanced */
+	/* 3. Get the balance factor of this
+		ancestor node to check whether
+		this node became unbalanced */
 	int balance = getBalance(node);
 
-	// If this node becomes unbalanced, then
-	// there are 4 cases
+	// If this node becomes unbalanced,
+	// then there are 4 cases
 
 	// Left Left Case
 	if (balance > 1 && key < node->left->key)
@@ -148,6 +151,129 @@ Node* insert(Node* node, int key)
 	return node;
 }
 
+/* Given a non-empty binary search tree,
+return the node with minimum key value
+found in that tree. Note that the entire
+tree does not need to be searched. */
+Node * minValueNode(Node* node)
+{
+	Node* current = node;
+
+	/* loop down to find the leftmost leaf */
+	while (current->left != NULL)
+		current = current->left;
+
+	return current;
+}
+
+// Recursive function to delete a node
+// with given key from subtree with
+// given root. It returns root of the
+// modified subtree.
+Node* deleteNode(Node* root, int key)
+{
+	
+	// STEP 1: PERFORM STANDARD BST DELETE
+	if (root == NULL)
+		return root;
+
+	// If the key to be deleted is smaller
+	// than the root's key, then it lies
+	// in left subtree
+	if ( key < root->key )
+		root->left = deleteNode(root->left, key);
+
+	// If the key to be deleted is greater
+	// than the root's key, then it lies
+	// in right subtree
+	else if( key > root->key )
+		root->right = deleteNode(root->right, key);
+
+	// if key is same as root's key, then
+	// This is the node to be deleted
+	else
+	{
+		// node with only one child or no child
+		if( (root->left == NULL) ||
+			(root->right == NULL) )
+		{
+			Node *temp = root->left ?
+						root->left :
+						root->right;
+
+			// No child case
+			if (temp == NULL)
+			{
+				temp = root;
+				root = NULL;
+			}
+			else // One child case
+			*root = *temp; // Copy the contents of
+						// the non-empty child
+			free(temp);
+		}
+		else
+		{
+			// node with two children: Get the inorder
+			// successor (smallest in the right subtree)
+			Node* temp = minValueNode(root->right);
+
+			// Copy the inorder successor's
+			// data to this node
+			root->key = temp->key;
+
+			// Delete the inorder successor
+			root->right = deleteNode(root->right,
+									temp->key);
+		}
+	}
+
+	// If the tree had only one node
+	// then return
+	if (root == NULL)
+	return root;
+
+	// STEP 2: UPDATE HEIGHT OF THE CURRENT NODE
+	root->height = 1 + max(height(root->left),
+						height(root->right));
+
+	// STEP 3: GET THE BALANCE FACTOR OF
+	// THIS NODE (to check whether this
+	// node became unbalanced)
+	int balance = getBalance(root);
+
+	// If this node becomes unbalanced,
+	// then there are 4 cases
+
+	// Left Left Case
+	if (balance > 1 &&
+		getBalance(root->left) >= 0)
+		return rightRotate(root);
+
+	// Left Right Case
+	if (balance > 1 &&
+		getBalance(root->left) < 0)
+	{
+		root->left = leftRotate(root->left);
+		return rightRotate(root);
+	}
+
+	// Right Right Case
+	if (balance < -1 &&
+		getBalance(root->right) <= 0)
+		return leftRotate(root);
+
+	// Right Left Case
+	if (balance < -1 &&
+		getBalance(root->right) > 0)
+	{
+		root->right = rightRotate(root->right);
+		return leftRotate(root);
+	}
+
+	return root;
+}
+
 // A utility function to print preorder
 // traversal of the tree.
 // The function also prints height
@@ -165,30 +291,51 @@ void preOrder(Node *root)
 // Driver Code
 int main()
 {
-	Node *root = NULL;
-	
+Node *root = NULL;
+
 	/* Constructing tree given in
 	the above figure */
+	root = insert(root, 9);
+	root = insert(root, 5);
 	root = insert(root, 10);
-	root = insert(root, 20);
-	root = insert(root, 30);
-	root = insert(root, 40);
-	root = insert(root, 50);
-	root = insert(root, 25);
-	
+	root = insert(root, 0);
+	root = insert(root, 6);
+	root = insert(root, 11);
+	root = insert(root, -1);
+	root = insert(root, 1);
+	root = insert(root, 2);
+
 	/* The constructed AVL Tree would be
-				30
-			/ \
-			20 40
-			/ \ \
-		10 25 50
+			9
+		/ \
+		1 10
+		/ \ \
+	0 5 11
+	/ / \
+	-1 2 6
 	*/
+
 	cout << "Preorder traversal of the "
 			"constructed AVL tree is \n";
 	preOrder(root);
-	
+
+	root = deleteNode(root, 10);
+
+	/* The AVL Tree after deletion of 10
+			1
+		/ \
+		0 9
+		/ / \
+	-1 5	 11
+		/ \
+		2 6
+	*/
+
+	cout << "\nPreorder traversal after"
+		<< " deletion of 10 \n";
+	preOrder(root);
+
 	return 0;
 }
 
-// This code is contributed by
-// rathbhupendra
+// This code is contributed by rathbhupendra
--- map/srcs/avl.hpp
+++ map/srcs/avl.hpp
@@ -4,100 +4,105 @@
 
 namespace ft
 {
-	struct Node
+	template< typename T >
+	class Node
 	{
-		int		key;
-		Node	*left;
-		Node	*right;
-		int		height;
-	};
-
-	class avl
-	{
-		private:
-
-			Node	*_tree;
-
 		public:
-		// --------------------------------
+			T		value;
+			Node	*left;
+			Node	*right;
+			int		height;
 
-			Node*	newNode(int key)
-			{
-				Node* n = new Node();
-				n->height = 1;
-				n->left = NULL;
-				n->right = NULL;
-				n->key = key;
-				return (n);
-			}
-			int		getHeight(Node *node) {
-				if (node == NULL)
-					return 0;
-				return node->height;
-			};
-			int		getBalance(Node *node)
-			{
-				if (node == NULL)
-					return 0;
-				return (getHeight(node->left) - getHeight(node->right));
-			}
-			Node*	rightRotate(Node* n)
-			{
-				Node *x = n->left;
-				Node *y = x->right;
-
-				x->right = n;
-				n->left = y;
-
-				n->height = 1 + std::max(getHeight(n->left), getHeight(n->right));
-				x->height = 1 + std::max(getHeight(x->left), getHeight(x->right));
-				return x;
-			}
-			Node*	leftRotate(Node* n)
-			{
-				Node *y = n->right;
-				Node *x = y->left;
-
-				y->left = n;
-				n->right = x;
-
-				n->height = 1 + std::max(getHeight(n->left), getHeight(n->right));
-				y->height = 1 + std::max(getHeight(y->left), getHeight(y->right));
-				return y;
-			}
-			Node*	insert(Node *node, int key)
-			{
-				if (node == NULL)
-					return (newNode(key));
-				if (key < node->key)
-					node->left = insert(node->left, key);
-				else if (key > node->key)
-					node->right = insert(node->right, key);
-				else
-					return node;
-				node->height = 1 + (std::max(getHeight(node->left), getHeight(node->right)));
-				int balance = getBalance(node);
-
-				if (balance > 1 && key < node->left->key) // left left case
-					return rightRotate(node);
-				if (balance < -1 && key > node->right->key) // right right case
-					return leftRotate(node);
-				if (balance > 1 && key > node->left->key) // left right case
-				{
-					node->left = leftRotate(node->left);
-					return rightRotate(node);
-				}
-				if (balance < -1 && key < node->right->key) // right left case
-				{
-					node->right = rightRotate(node->right);
-					return leftRotate(node);
-				}
-
-				return node;
-			}
-
-		// --------------------------------
-			avl () : _tree(NULL) {};
-			~avl() {};
+			Node(const T &value): value(value), left(NULL), right(NULL), height(1) {};
+			~Node() {};
 	};
+
+	// class avl
+	// {
+	// 	private:
+
+	// 		Node	*_tree;
+
+	// 	public:
+	// 	// --------------------------------
+
+	// 		Node*	newNode(int key)
+	// 		{
+	// 			Node* n = new Node();
+	// 			n->height = 1;
+	// 			n->left = NULL;
+	// 			n->right = NULL;
+	// 			n->key = key;
+	// 			return (n);
+	// 		}
+	// 		int		getHeight(Node *node) {
+	// 			if (node == NULL)
+	// 				return 0;
+	// 			return node->height;
+	// 		};
+	// 		int		getBalance(Node *node)
+	// 		{
+	// 			if (node == NULL)
+	// 				return 0;
+	// 			return (getHeight(node->left) - getHeight(node->right));
+	// 		}
+	// 		Node*	rightRotate(Node* n)
+	// 		{
+	// 			Node *x = n->left;
+	// 			Node *y = x->right;
+
+	// 			x->right = n;
+	// 			n->left = y;
+
+	// 			n->height = 1 + std::max(getHeight(n->left), getHeight(n->right));
+	// 			x->height = 1 + std::max(getHeight(x->left), getHeight(x->right));
+	// 			return x;
+	// 		}
+	// 		Node*	leftRotate(Node* n)
+	// 		{
+	// 			Node *y = n->right;
+	// 			Node *x = y->left;
+
+	// 			y->left = n;
+	// 			n->right = x;
+
+	// 			n->height = 1 + std::max(getHeight(n->left), getHeight(n->right));
+	// 			y->height = 1 + std::max(getHeight(y->left), getHeight(y->right));
+	// 			return y;
+	// 		}
+	// 		Node*	insert(Node *node, int key)
+	// 		{
+	// 			if (node == NULL)
+	// 				return (newNode(key));
+	// 			if (key < node->key)
+	// 				node->left = insert(node->left, key);
+	// 			else if (key > node->key)
+	// 				node->right = insert(node->right, key);
+	// 			else
+	// 				return node;
+	// 			node->height = 1 + (std::max(getHeight(node->left), getHeight(node->right)));
+	// 			int balance = getBalance(node);
+
+	// 			if (balance > 1 && key < node->left->key) // left left case
+	// 				return rightRotate(node);
+	// 			if (balance < -1 && key > node->right->key) // right right case
+	// 				return leftRotate(node);
+	// 			if (balance > 1 && key > node->left->key) // left right case
+	// 			{
+	// 				node->left = leftRotate(node->left);
+	// 				return rightRotate(node);
+	// 			}
+	// 			if (balance < -1 && key < node->right->key) // right left case
+	// 			{
+	// 				node->right = rightRotate(node->right);
+	// 				return leftRotate(node);
+	// 			}
+
+	// 			return node;
+	// 		}
+
+	// 	// --------------------------------
+	// 		avl () : _tree(NULL) {};
+	// 		~avl() {};
+	// };
 }
