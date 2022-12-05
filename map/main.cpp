defd34d9fba9e16e444d6439c29347006e68a2f5
Abdellah El Azraoui <ael-azra@student.1337.ma>
2022-12-05 20:50:16 +0100

    abdellah

--- map/main.cpp
+++ map/main.cpp
@@ -1,4 +1,6 @@
 #include "./map.hpp"
+#include <map>
+#include <vector>
 
 
 
@@ -15,5 +17,13 @@ int main()
 		m.insert(ft::make_pair(1, 1337));
 		m.insert(ft::make_pair(2, 1337));
 		std::cout << m[99] << std::endl;
+
+		m[99] = 1337;
+		ft::map<int, int>::iterator it;
+		for (it = m.begin(); it != m.end(); it++)
+		{
+			std::cout << "key = " << it->first << " value = " << it->second << std::endl;
+		}
+		// std::map<int, int> 
 	return 0;
 }
