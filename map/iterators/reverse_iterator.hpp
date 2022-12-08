17e99237fd061bfc4e7d701b53dcb00fb9807497
Azraoui <ael-azra@student.1337.ma>
2022-12-08 12:22:14 +0100

    abdellah

--- map/iterators/reverse_iterator.hpp
+++ map/iterators/reverse_iterator.hpp
@@ -17,7 +17,7 @@ namespace ft
 			typedef	typename	ft::Iterator_traits<Iterator>::reference			reference;
 		
 		private:
-			//
+			iterator_type	__base_it;
 
 		public:
 			// Member functions
@@ -27,28 +27,24 @@ namespace ft
 			 * 	Constructs a reverse iterator that points to no object.
 			 * The internal base iterator is value-initialized.
 			 */
-			reverse_iterator() {
-
-			};
+			reverse_iterator() : __base_it() {};
 			/**
 			 * @brief initialization constructor
 			 * Constructs a reverse iterator from some original iterator it.
 			 * The behavior of the constructed object replicates the original,
 			 * except that it iterates through its pointed elements in the reverse order.
 			 */
-			explicit reverse_iterator (iterator_type it)
-			{
-
-			};
+			explicit reverse_iterator (iterator_type it) : __base_it(it) {};
 			/**
 			 * @brief  copy / type-cast constructor
 			 * Constructs a reverse iterator from some other reverse iterator.
 			 * The constructed object keeps the same sense of iteration as rev_it.
 			 */
 			template <class Iter>
-			reverse_iterator (const reverse_iterator<Iter>& rev_it)
-			{
-
-			};
+			reverse_iterator (const reverse_iterator<Iter>& rev_it) : __base_it(rev_it.__base_it) {};
+			/**
+			 * @brief Destructor
+			 */
+			~reverse_iterator() {};
 	};
 }
