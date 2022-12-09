f15231636022edf321614678b086507d058ccce0
Azraoui <ael-azra@student.1337.ma>
2022-12-09 15:39:39 +0100

    abdellah

--- learn.cpp
+++ learn.cpp
@@ -4,24 +4,25 @@
 #include <vector>       // std::vector
 
 int main () {
-  std::vector<int> myvector;
-  for (int i=0; i<10; i++) myvector.push_back(i);
+  // std::vector<int> myvector;
+  // for (int i=0; i<10; i++) myvector.push_back(i);
 
-  typedef std::vector<int>::iterator iter_type;
-                                                         // ? 9 8 7 6 5 4 3 2 1 0 ?
-  iter_type from (myvector.begin());                     //   ^
-                                                         //         ------>
-  iter_type until (myvector.end());                      //                       ^
-                                                         //
-  std::reverse_iterator<iter_type> rev_until (from);     // ^
-                                                         //         <------
-  std::reverse_iterator<iter_type> rev_from (until);     //                     ^
+  // typedef std::vector<int>::iterator iter_type;
+  //                                                        // ? 9 8 7 6 5 4 3 2 1 0 ?
+  // iter_type from (myvector.begin());                     //   ^
+  //                                                        //         ------>
+  // iter_type until (myvector.end());                      //                       ^
+  //                                                        //
+  // std::reverse_iterator<iter_type> rev_until (from);     // ^
+  //                                                        //         <------
+  // std::reverse_iterator<iter_type> rev_from (until);     //                     ^
 
-  std::cout << "myvector:";
-  while (rev_from != rev_until)
-    std::cout << ' ' << *rev_from++;
-  std::cout << '\n';
+  // std::cout << "myvector:";
+  // while (rev_from != rev_until)
+  //   std::cout << ' ' << *rev_from++;
+  // std::cout << '\n';
   // std::cout << from[-3] << std::endl;
+  std::cout << std::is_same<int, long int>::value << std::endl;
 
   return 0;
 }
--- map/utils/utils.hpp
+++ map/utils/utils.hpp
@@ -9,12 +9,44 @@ namespace ft
 	{
 		return ( ft::pair<T1,T2>(x,y) );
 	}
+
 	template <class T>
-	class is_integral
+	struct is_integral {const bool	value = false;};
+	template <>
+	struct is_integral<bool>{ const bool	value = true;};
+	template <>
+	struct is_integral<char>{ const bool value = true;};
+	template <>
+	struct is_integral<char16_t>{ const bool value = true;};
+	template <>
+	struct is_integral<char32_t>{ const bool value = true;};
+	template <>
+	struct is_integral<wchar_t>{ const bool value = true;};
+	template <>
+	struct is_integral<signed char>{ const bool value = true;};
+	template <>
+	struct is_integral<short int>{ const bool value = true;};
+	template <>
+	struct is_integral<int>{ const bool value = true;};
+	template <>
+	struct is_integral<long int>{ const bool value = true;};
+	template <>
+	struct is_integral<long long int>{ const bool value = true;};
+	template <>
+	struct is_integral<unsigned char>{ const bool value = true;};
+	template <>
+	struct is_integral<unsigned short int>{ const bool value = true;};
+	template <>
+	struct is_integral<unsigned int>{ const bool value = true;};
+	template <>
+	struct is_integral<unsigned long int>{ const bool value = true;};
+	template <>
+	struct is_integral<unsigned long long int>{ const bool value = true;};
+
+	template <bool Cond, class T = void>
+	struct enable_if
 	{
-		public:
-			typedef bool	value_type;
-			typedef	T		type;
-			value_type		value;
+		
 	};
+
 }
